a69ae5b5b2fb4cf86ea1b77bf59b4ebc84c1a16d
wataru sasaki <make.it.sskwtr.40@gmail.com>
2020-11-06 22:07:01 +0900

    random

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # Flaskからimportしてflaskを使えるようにする。
 from flask import Flask, render_template
 import sqlite3
+import random
 
 # appっていう名前でFlaskアプリを作っていくよーみたいな
 app = Flask(__name__)
@@ -8,8 +9,7 @@ app = Flask(__name__)
 # 秘密鍵
 app.secret_key = "kimazuin"
 
-<<<<<<< HEAD
-=======
+
 @app.route("/dbtest")
 def dbtest():
     # flask.dbに接続
@@ -20,11 +20,12 @@ def dbtest():
     c.execute("select * from places")
     # 取ってきたレコードを格納 fetch=取ってくるという意味の英単語
     places = c.fetchone()
+    print()
     # データベース接続終了
     c.close()
-    
+
     return render_template("dbtest.html", places=places)
->>>>>>> d2ca1d0373683e6d5382e267b87599dbffbda951
+
 
 @app.route("/")
 def helloWorld():
@@ -38,7 +39,30 @@ def template():
 
 @app.route("/question")
 def question():
-    return render_template("question.html")
+    conn = sqlite3.connect("flask.db")
+    c = conn.cursor()
+    contents = "SELECT content, advice FROM questions WHERE place_id = 1"
+    c.execute(contents)
+    contents = c.fetchall()
+    content = contents[0]
+    advice = contents[1]
+    content = random.choice(contents)
+    advice = random.choice(contents)
+    c.close()
+
+    conn = sqlite3.connect("flask.db")
+    c = conn.cursor()
+    places = "SELECT place FROM places WHERE id = 1"
+    c.execute(places)
+    places = c.fetchall()
+    place = places[0]
+    place = random.choice(places)
+    c.close()
+    # c.execute(places)
+    # place = c.fetchone()
+    # places = random.choice(place)
+    # c.close()
+    return render_template("question.html", content=content, place=place)
 
 
 @app.route("/star")
@@ -54,3 +78,5 @@ def notfound(code):
 if __name__ == "__main__":
     # Flaskが持っている開発用サーバーを実行します。
     app.run(debug=True)
+
+# join places on questions.place_id = places.id
