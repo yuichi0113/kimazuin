a235de1831bdd9a615e39df149343af919ecad76
hrd3a2577 <hrdea2577@gmail.com>
2020-11-09 19:23:13 +0900

    added  css

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Flaskからimportしてflaskを使えるようにする。
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import sqlite3
 import random
 
@@ -20,10 +20,10 @@ def dbtest():
     c.execute("select * from places")
     c.execute("select * from places")
     # 取ってきたレコードを格納 fetch=取ってくるという意味の英単語
-    places = c.fetchall()
+    places = c.fetchone
     # データベース接続終了
     c.close()
-    
+
     return render_template("dbtest.html", places=places)
 
 
@@ -39,10 +39,11 @@ def dbtest2():
     content_li = c.fetchall()
     # データベースの接続終了
     c.close
-    
-    content=random.choice(content_li)
+
+    content = random.choice(content_li)
 
     return render_template("dbtest2.html", content=content[0], advice=content[1])
+
 
 @app.route("/")
 def helloWorld():
@@ -56,12 +57,49 @@ def template():
 
 @app.route("/question")
 def question():
-    return render_template("question.html")
+    conn = sqlite3.connect("flask.db")
+    c = conn.cursor()
+    contents = "SELECT content, advice FROM questions WHERE place_id = 1"
+    c.execute(contents)
+    contents = c.fetchall()
+    content = contents[0]
+    advice = contents[1]
+    content = random.choice(contents)
+    advice = random.choice(contents)
+    c.close()
+
+    return render_template("question.html", content=content, place=advice)
 
 
-@app.route("/star")
-def achievement():
+@app.route("/star", methods=["GET"])
+def achievement_get():
     return render_template("star.html")
+
+
+@app.route("/star", methods=["POST"])
+def achievement_post():
+    star = request.form.get("star")
+    comment = request.form.get("comment")
+    conn = sqlite3.connect('flasktest.db')
+    c = conn.cursor()
+    c.execute("insert into questions values(star,comment)", (star, comment))
+    conn.commit()
+    conn.close()
+    return redirect("/date")
+
+
+@app.route("/date")
+def date():
+    conn = sqlite3.connect('flasktest.db')
+    c = conn.cursor()
+    c.execute = "select content,advice,star,comment from questions where id = ?", (
+        id,)
+    date_list = []
+    for row in c.fetchall():
+        date_list.append(
+            {"content": row[0], "advice": row[1], "star": row[2], "comment": row[3]})
+    c.close()
+    return render_template('date.html', date_list=date_list)
 
 
 @app.errorhandler(404)
@@ -72,3 +110,5 @@ def notfound(code):
 if __name__ == "__main__":
     # Flaskが持っている開発用サーバーを実行します。
     app.run(debug=True)
+
+# join places on questions.place_id = places.id
